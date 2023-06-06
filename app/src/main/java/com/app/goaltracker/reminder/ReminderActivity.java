a8628e9d265ae824bd998c6d090438b399b968e9
Henrik Gardishyan <henrik.gardishyan@gmail.com>
2023-06-07 00:47:10 +0400

    Added a couple of comments

--- app/src/main/java/com/app/goaltracker/reminder/ReminderActivity.java
+++ app/src/main/java/com/app/goaltracker/reminder/ReminderActivity.java
@@ -1,20 +1,25 @@
 package com.app.goaltracker.reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import com.app.goaltracker.R;
+import com.app.goaltracker.mvvm.GoalsViewModel;
 
 public class ReminderActivity extends AppCompatActivity {
+    private GoalsViewModel goalsViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
 
-        //    List<GoalWithHistory> goalList =
-
-
+        goalsViewModel = new ViewModelProvider(this).get(GoalsViewModel.class);
+        goalsViewModel.getLiveGoals().observe(this, goals -> {
+            //1. filter goals to leave the ones that have reminder now
+            //2. create event for each goal and put into HistoryAdapter
+        });
     }
 }
